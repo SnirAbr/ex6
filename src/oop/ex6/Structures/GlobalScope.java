46eaccecfa7c76fa7e8fb037e6c850c0377d31a5
SnirAbr <snir987654@gmail.com>
2018-06-17 09:59:58 +0300

    done with variables detection

--- src/oop/ex6/Structures/GlobalScope.java
+++ src/oop/ex6/Structures/GlobalScope.java
@@ -1,4 +1,4 @@
-package oop.ex6.Structures;
+package oop.ex6.structures;
 
 import java.util.*;
 
--- src/oop/ex6/Structures/Variable.java
+++ src/oop/ex6/Structures/Variable.java
@@ -1,14 +1,21 @@
-package oop.ex6.Structures;
+package oop.ex6.structures;
 
 public class Variable {
 
-	private String name;
-	private Type type;
-	private String value;
+    private String name;
+    private String type;
+    private String value;
+    private boolean isFinal;
 
-	public Variable(String name, Type type, String value){
-		this.name = name;
-		this.type = type;
-		this.value = value;
-	}
+    public Variable(boolean isFinal, String name, String type, String value) {
+        this.name = name;
+        this.type = type;
+        this.value = value;
+        this.isFinal = isFinal;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
